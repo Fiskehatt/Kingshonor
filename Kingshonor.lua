f131d5093b03c30a95c16e644ae83825adbbb264
Coax <Fiskehatt@users.noreply.github.com>
2019-09-26 02:18:59 +0300

    remove comments

--- Kingshonor.lua
+++ Kingshonor.lua
@@ -1,7 +1,7 @@
-local kingframe=CreateFrame("Frame");-- Need a frame to capture events
-kingframe:RegisterEvent("CHAT_MSG_SAY");-- Register our event
-kingframe:SetScript("OnEvent",function(self,event,msg)-- OnEvent handler receives event triggers
-    if event=="CHAT_MSG_SAY" then-- Someone sends "hello World" in /say
+local kingframe=CreateFrame("Frame");
+kingframe:RegisterEvent("CHAT_MSG_SAY");
+kingframe:SetScript("OnEvent",function(self,event,msg)
+    if event=="CHAT_MSG_SAY" then
         local message = strlower(msg);
         if (strfind(message, "kings") ~= nil or strfind(message, "king's") ~= nil) and (strfind(message, "honor") ~= nil or strfind(message, "honour") ~= nil) and strfind(message, "friend") ~= nil then
             PlaySoundFile("Interface\\AddOns\\Kingshonor\\552194.ogg", "Master")
